fd876828a7b35744b4fe04bdd895a30daed4dbfa
Gabriel123789 <bispogabriel398@gmail.com>
2026-06-08 11:15:42 -0300

    Add files via upload

---
+++ README.md/js/app.js
@@ -0,0 +1,14 @@
+
+
+
+alert('Boas vindas ao jogo do número secreto');
+let numeroScreto = 144;
+console.log(numeroSecreto)
+let chute = prompt(' Escolha um número entre 1 e 200')
+
+// se chete for igual ao número secreto
+if (numeroSecreto == chute) {
+    alert('isso ai! Você descobriu o número secreto (5)');
+} else {
+    alert('Você errou :')
+}
